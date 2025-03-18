4083039cb4d591261a6d40671adb0e0bae4cb58a
kvvyas <154830731+kvvyas@users.noreply.github.com>
2025-03-18 17:03:33 -0400

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import {Route,BrowserRouter as Router,Routes} from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <main className = "bg-slate-300/20">
+        <Router>
+           <Navbar /> 
+           <Routes>
+             <Route path = "/"element = {"Home"} />
+             <Route path = "/about" element = {'About'}/>
+             <Route path = "/competition" element = {'competition'}/>
+             <Route path = "/projects" element = {'projects'}/>
+             <Route path = "/contact" element = {'contact'}/>
+           </Routes>
+        </Router>
+    </main>
+  )
+}
+
+export default App
